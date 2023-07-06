6b411d76818b51aac3945cf1ed30b6bb4e3af75f
Javier Zavala <javier.zavala@honeywell.com>
2023-07-06 12:51:22 -0600

    update

--- archivo_sesion_jueves.c
+++ archivo_sesion_jueves.c
@@ -1,4 +1,6 @@
-#include <string.h>
+//git clone https://github.com/javierzavalaponce/honeywell_1stweek.git
+//0. mision: Cual es el tamaño del arreglo y su direccion ram?
+//arreglo consume rom?
 char arreglo[] = {
 "Everybody knows that the boat is leaking Everybody knows that the captain lied"
 };
@@ -6,8 +8,40 @@ char arreglo[] = {
 #pragma optimize=none
 int main()
 {
-  unsigned int resultado=0;
-  resultado =  sizeof(arreglo);
-  resultado = (unsigned int)arreglo;
-  return resultado;
+  unsigned int direccion=0; //tamaño de tipo uint?
+  volatile unsigned int tamano=0;
+  tamano =  sizeof(arreglo);
+  tamano =  sizeof(unsigned int);
+  arreglo[0] = '3';
+  *(arreglo+4) = 1; //* lo que tiene adx NOMBRE del arreglo es..
+  *(unsigned int*)(arreglo+4) = 2;
+  direccion = (unsigned int)arreglo; //1. Que hace esta linea
+  return direccion; //2. Que hacer para retornar dos variables o mas?
 }
+//*hint: cast
+
+
+
+
+
+
+
+
+//unsigned int dec(unsigned int input)
+//{
+//  input --;
+//  return input;
+//}
+//unsigned int func(unsigned int input)
+//{
+//  input++; // detener aqui
+//  dec(input);
+//  return input;
+//}
+//
+//#pragma optimize=none
+//int main()
+//{
+//  unsigned int res=93;
+//  return func(2);
+//}
